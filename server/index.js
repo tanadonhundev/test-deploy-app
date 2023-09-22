9362f2f1c2cd2db1b7c8036cd431ef243d53e409
tanadondev <142754243+tanadonhundev@users.noreply.github.com>
2023-09-22 23:30:24 +0700

    a

--- server/index.js
+++ server/index.js
@@ -1,23 +1,14 @@
+// Import packages
 const express = require("express");
-const shorturlRoutes = require('./routes/shorturl');
+const home = require("./routes/home");
 
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
-const connectDB = require("./configs/database");
-
-const { readdirSync } = require("fs");
-
+// Middlewares
 const app = express();
+app.use(express.json());
 
+// Routes
+app.use("/home", home);
 
-connectDB();
-
-app.use(cors());
-app.use(bodyParser.json());
-
-//route
-app.use('/shorturls', shorturlRoutes);
-
-const port = process.env.PORT||5000;
-app.listen(port, () => console.log('Server Running on Port ' + port))
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
