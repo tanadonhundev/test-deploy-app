af03abe8380ff6dc5963211bd7de8f2529a7e09d
tanadondev <142754243+tanadonhundev@users.noreply.github.com>
2023-09-23 02:22:50 +0700

    1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,9 +4,9 @@ const auth = require('../controllers/auth')
 
 
 //http://localhost:5000/api/register
-router.post("/register", auth.registerUser);
+router.post("/api/register", auth.registerUser);
 
-router.post("/login", auth.loginUser);
+router.post("/api/login", auth.loginUser);
 
 
 module.exports = router;
