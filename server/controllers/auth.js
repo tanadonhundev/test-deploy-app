135a3efcee663648f3887f75229867c3ee789c31
tanadondev <142754243+tanadonhundev@users.noreply.github.com>
2023-09-23 04:51:00 +0700

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -10,24 +10,22 @@ async function registerUser(req, res, next) {
         if (user) {
             return res.send("มีผู้ใช้งานในระบบแล้ว");
         }
-        else {
-            //Encrypt
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(password, salt)
+        //Encrypt
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
 
-            const newUser = new User({
-                firstName,
-                lastName,
-                email,
-                password: hashedPassword,
-            });
-            //Save in Database
-            await newUser.save();
-            res.send("สมัครสมาชิกสำเร็จแล้ว");
-        }
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+        });
+        //Save in Database
+        await newUser.save();
+        res.send("สมัครสมาชิกสำเร็จแล้ว");
     } catch (error) {
         console.log(error);
-        res.send("Server Error");
+        res.send("Se");
     }
 };
 
