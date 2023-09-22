8c1622c5b02fe81ea56f33003a602f868be1e262
tanadondev <142754243+tanadonhundev@users.noreply.github.com>
2023-09-22 23:41:32 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-// Import packages
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./configs/database");
+
 const home = require("./routes/home");
 const mainRoutes = require("./routes/mainRoutes");
 
+connectDB();
+
 // Middlewares
 const app = express();
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
 // Routes
 app.use("/home", home);
