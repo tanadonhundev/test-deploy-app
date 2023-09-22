56caf7227e304fa88fd4eb34a39b37d0c5d3318c
tanadondev <142754243+tanadonhundev@users.noreply.github.com>
2023-09-22 23:20:49 +0700

    up

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
+
+//"mongodb+srv://thanadev:1234@cluster0.z6ys8ya.mongodb.net/ShortURL"
+const url = process.env.DATABASE;
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://thanadev:1234@cluster0.z6ys8ya.mongodb.net/ShortURL");
+        await mongoose.connect(url);
 
         console.log("DB Connecte")
     } catch (error) {
