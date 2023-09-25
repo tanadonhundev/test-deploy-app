c3bb4d9b5e0dbd3bae8517fdf0d3438a9e2895b6
tanadondev <142754243+tanadonhundev@users.noreply.github.com>
2023-09-25 16:05:22 +0700

    Update root.jsx

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -3,8 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../components/pages/home-page";
 import RegisterPage from "../components/pages/register-page";
 import LoginPage from "../components/pages/login-page";
-
-
+import ShorturlPage from "../components/pages/shorturl-page";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +18,10 @@ const router = createBrowserRouter([
     path: "/login",
     element: <LoginPage />,
   },
-
+  {
+    path: "/shorturl",
+    element: <ShorturlPage />,
+  },
 ]);
 
 export default router;
